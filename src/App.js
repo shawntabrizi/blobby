026811a32bc568d27a2dc0885fec366bcaa75589
Shawn Tabrizi <shawntabrizi@gmail.com>
2023-12-21 21:49:12 -0400

    basic connect and block number

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { SubstrateProvider } from './SubstrateContext';
+import BlockNumberComponent from './BlockNumber';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SubstrateProvider providerUrl="wss://rpc.polkadot.io">
+        <div>
+          <h1>Your App</h1>
+          <BlockNumberComponent />
+          {/* Other components that need access to the Substrate API */}
+        </div>
+      </SubstrateProvider>
+
+      <SubstrateProvider providerUrl="wss://kusama-rpc.polkadot.io">
+        <div>
+          <h1>Your App</h1>
+          <BlockNumberComponent />
+          {/* Other components that need access to the Substrate API */}
+        </div>
+      </SubstrateProvider>
     </div>
   );
 }
---
+++ src/BlockNumber/index.js
@@ -0,0 +1,43 @@
+// BlockNumberComponent.js
+import React, { useEffect, useState } from 'react';
+import { useSubstrate } from '../SubstrateContext';
+
+const BlockNumberComponent = () => {
+  const { api } = useSubstrate();
+  const [blockNumber, setBlockNumber] = useState(null);
+
+  useEffect(() => {
+    // Fetch the latest block number and subscribe to new blocks
+    const fetchAndSubscribe = async () => {
+      try {
+        if (api) {
+          // Fetch initial block number
+          const blockHeader = await api.rpc.chain.getHeader();
+          setBlockNumber(blockHeader.number.toNumber());
+
+          // Subscribe to new blocks
+          api.rpc.chain.subscribeNewHeads((header) => {
+            setBlockNumber(header.number.toNumber());
+          });
+        }
+      } catch (error) {
+        console.error('Error fetching and subscribing to block number:', error);
+      }
+    };
+
+    fetchAndSubscribe();
+  }, [api]);
+
+  return (
+    <div>
+      <h2>Block Number Component</h2>
+      {blockNumber !== null ? (
+        <p>Latest Block Number: {blockNumber}</p>
+      ) : (
+        <p>Fetching block number...</p>
+      )}
+    </div>
+  );
+};
+
+export default BlockNumberComponent;
